837194f955c8b8dbb825fbdcad738ca3645e8a7c
Scott Wagner <af17317596@gmail.com>
2019-04-16 15:32:07 -0500

    getting started

---
+++ controllers/routes.js
@@ -0,0 +1,22 @@
+//
+var express = require("express");
+var router = express.Router();
+
+//route to display our index.handlebars (homepage)
+router.get("/", function(req, res){
+res.status(200);
+});
+
+
+// route to create a burger (post request)
+router.post("/create", function(req, res){
+  res.status(200);
+})
+
+// route to update the burger throught its id]
+router.put("/:id", function(req, res){
+  res.status(200);
+})
+
+
+// export your routes: 
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express")
+// var methodOverride = require("method-override")
+var exphbs = require("express-handlebars")
+var routes = require('./controllers/routes.js')
+
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+app.use(express.static("public"));
+
+//Middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//handelbars as view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Pass routes inside of express
+app.use(routes);
+
+
+app.listen(PORT, function(){
+    console.log('Listening on port: ', PORT)
+})
