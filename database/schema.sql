3548d86bab622d2369684988df9e3f78bc063105
Scott Wagner <af17317596@gmail.com>
2019-04-17 12:08:32 -0500

    DATA"T" base fix

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-CREATE DATATBASE burgerDB;
+CREATE DATABASE IF NOT EXISTS burgerDB;
 
 USE burgerDB;
 
@@ -7,9 +7,10 @@ CREATE TABLE burger (
     name varchar(30) NOT NULL,
     calories INT NOT NULL,
     price DECIMAL NOT NULL,
+    eaten BOOL DEFAULT false,
     PRIMARY KEY(id)
 );
 
-INSERT INTO burger (name, calories, price) VALUES ("Santa Fe, 1200, 10.00")
-INSERT INTO burger (name, calories, price) VALUES ("Double Stack, 1700, 12.00")
-INSERT INTO burger (name, calories, price) VALUES ("Juicy Lucy, 2000, 13.00")
+INSERT INTO burger (name, calories, price, eaten) VALUES ("Santa Fe", 1200, 10.00, FALSE);
+INSERT INTO burger (name, calories, price, eaten) VALUES ("Double Stack", 1700, 12.00, FALSE);
+INSERT INTO burger (name, calories, price, eaten) VALUES ("Juicy Lucy", 2000, 13.00, FALSE);
