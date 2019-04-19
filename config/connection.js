8b9b0fe37b60b1e1322f256a948d9d63017f6ec2
Scott Wagner <af17317596@gmail.com>
2019-04-19 00:56:52 -0500

    final commit

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgerDB"
-});
+if(process.env.JAWSB_URL){
+connection.mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgerDB"
+  });
+}
+
 
 /*
-if (process.env.MYSQL_URL) {
-  var connection = mysql.createConnection(process.env.MYSQL_URL);
-  host = 'MYSQL';
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+  host = 'JAWSDB';
 } else {
   connection = mysql.createConnection(config);
   host = 'localhost';
