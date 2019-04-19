9591bd15915ee73dfb40b6700ed37abc5253a751
Scott Wagner <af17317596@gmail.com>
2019-04-19 14:30:34 -0500

    first commit

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,33 @@
+var dotenv = require("dotenv");
+dotenv.config();
 var mysql = require("mysql");
-var connection;
 
-if(process.env.JAWSDB_URL){
-connection.mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
+var {DB_USER, DB_PASS, DB_NAME} = process.env;
+var config = { 
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgerDB"
-  });
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME
 }
+var connection;
+var host;
 
-
-/*
-if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-  host = 'JAWSDB';
-} else {
+if(process.env.JAWSDB_URL){
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
   connection = mysql.createConnection(config);
-  host = 'localhost';
+  host = "localhost";
 }
-*/
+
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected with " + host);
 });
 
 module.exports = connection;
